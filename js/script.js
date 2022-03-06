2a6f38cf4b8df426cc5ecc2a7d1fe6c7a4552e1a
Mohd NAim <mohdnaeem798@gmail.co>
2022-03-06 09:02:49 +0530

    my website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//typing animation 
+var typed = new Typed (".typing",{
+    strings:["","Web Designer","Free Lancer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    backSpeed:60, 
+    loop:true
+})
