6b7017b3de6aed2ad3ba54a717c47b7f787f9cea
Mohd-naim <mohdnaeem789@gmail.com>
2022-05-08 23:15:21 +0530

    update

--- sample.java
+++ sample.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class sample{
-    public static void main(args[] String){
-        System.out.println("Hello World!!!!!");
+    public static void main(String[] args){
+     //TAKE INPUT FROM USER
+        System.out.println("Enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println("Enter another number: ");
+        int num2 = input.nextInt();
+        System.out.println("Enter another number: ");
+        int num3 = input.nextInt();
+        System.out.println("Enter another number: ");
+        int num4 = input.nextInt();
+        //CLOSE INPUT
+        input.close();
+      
+        
+       
     }
 }
